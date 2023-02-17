742fd648be417c7d1e5603cb95f290c3fda3bd28
thiawndoumbe <thiawndoumbe99@gmail.com>
2023-02-17 11:58:07 +0000

    exercice1

---
+++ Ex1.py
@@ -0,0 +1,12 @@
+class Student:
+    def __init__(self,nom,filier,notes):
+        self.nom=nom
+        self.filier=filier
+        self.notes=notes
+    def operation(self,notes):
+        maxi = notes[0]
+        longueur=len(self.notes)
+        for i in range(longueur):
+            if self.notes[i] >= maxi:
+                maxi = notes[i]
+            return maxi
